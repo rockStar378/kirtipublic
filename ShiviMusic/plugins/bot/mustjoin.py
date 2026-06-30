5ecea4d7d9fcf1ea69391c19e90a0d0a4b8de0ea
rockStar378 <shahzadluqman092@gmail.com>
2026-07-01 00:55:21 +0530

    Update mustjoin.py

--- ShiviMusic/plugins/bot/mustjoin.py
+++ ShiviMusic/plugins/bot/mustjoin.py
@@ -9,7 +9,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from TEAMZYRO import app
+from ShiviMusic import app
 
 #--------------------------
 
