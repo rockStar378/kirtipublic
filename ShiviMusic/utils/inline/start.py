00e396dc8493e754441d2766aeff0b24c0a96abb
rockStar378 <shahzadluqman092@gmail.com>
2026-07-01 09:04:20 +0530

    Update start.py

--- ShiviMusic/utils/inline/start.py
+++ ShiviMusic/utils/inline/start.py
@@ -2,17 +2,12 @@
 # ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 🚀
 # 
 # This source code is under MIT License 📜
-# ❌ Unauthorized forking, importing, or using this code
-#    without giving proper credit will result in legal action ⚠️
-# 
-# 📩 DM for permission : @TheSigmaCoder
 # ===========================================================
 
 from pyrogram.types import InlineKeyboardButton
 
 import config
 from ShiviMusic import app
-
 
 
 def start_panel(_):
@@ -23,21 +18,7 @@ def start_panel(_):
 def private_panel(_):
     buttons = []
     return buttons
-    
-            InlineKeyboardButton(
-                text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_9"], callback_data="sbot_cb"),  
-            InlineKeyboardButton(text=_["S_B_13"], callback_data="abot_cb"),
-        ],
-        [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
-        ],
-    ]
-    return buttons
+
 
 # ===========================================================
 # ©️ 2025-26 All Rights Reserved by Purvi Bots (Im-Notcoder) 😎
